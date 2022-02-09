5a5899b5e78fe33982c62ed16c364f60c31e630f
Tecla Roig <ch.roig@gmail.com>
2022-02-09 18:45:28 -0300

    migrate html to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppBoxComponent } from './shared/app-box/app-box.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LinksComponent } from './pages/links/links.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppBoxComponent,
+    HomeComponent,
+    LinksComponent
   ],
   imports: [
     BrowserModule,
